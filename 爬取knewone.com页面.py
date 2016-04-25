e6645a51ee2a60bb34586825c6dc110aaa1b4dc2
ZGuangW <W_ZG@hotmail.com>
2016-04-26 00:56:01 +0800

    改进爬取knewone.com产品列表的模块 加入写入文档功能

--- 爬取knewone.com页面.py
+++ 爬取knewone.com页面.py
@@ -6,9 +6,12 @@ https://knewone.com/
 from bs4 import BeautifulSoup
 import requests
 import time
+import codecs
 
 url = 'https://knewone.com/discover?page='
 all_data = []
+txt_temp = ""
+
 
 # ===获取每页商品信息===
 def get_page(url):
@@ -34,11 +37,27 @@ def get_more_pages(start, end):
     for one in range(start, end + 1):
         print('正在解析第%s页' % str(one))
         all_data.extend(get_page(url + str(one)))
-        time.sleep(1)
+        if (one % 50 == 0):
+            time.sleep(10)
+        else:
+            time.sleep(1)
+
+
+# ===写入记录文档===
+def writeIntoText(data, file_name=None, txt_temp=None):
+    file_name = "knewone产品列表" + file_name + ".txt"
+    txt = codecs.open(file_name, "w", "utf-8")
+    for i in data:
+        txt_temp += str(i) + "\n"
+    txt.write(txt_temp)
+    txt.close()
+    print("生成完毕")
 
 
 if __name__ == '__main__':
     start = int(input('请输入起始页：'))
     end = int(input('请输入结束页：'))
+    file_name = str(input('请输入文件标识名：'))
     get_more_pages(start, end)
-    print(all_data)
+    writeIntoText(all_data, file_name, txt_temp)
+    # print(all_data)
